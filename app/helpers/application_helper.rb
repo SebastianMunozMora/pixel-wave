091757903693219c28f6f99e2048a9b56eabb2b9
Sebastian <smm9308@gmail.com>
2025-03-13 08:42:33 -0500

    feat(ux-text): redesign, limit words

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def truncate_words(text, word_limit)
+    words = text.split
+    if words.size > word_limit
+      words[0...word_limit].join(' ') + '...'
+    else
+      text
+    end
+  end
 end
