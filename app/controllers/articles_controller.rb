8555c359b9569831386941d6c7116cb31065a1fe
Sebastian <smm9308@gmail.com>
2025-06-21 10:14:33 -0500

    authentication: skip for show and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,8 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
   before_action :require_admin_or_editor, only: %i[new create edit update destroy]
 
+  skip_before_action :authenticate_user!, only: %i[index show]
+
   # GET /articles or /articles.json
   def index
     @articles = Article.all
